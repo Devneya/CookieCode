551cdadafaad8ce1c4297c158d2c236b2b3639c9
IgorianKiselev02 <51312865+IgorianKiselev02@users.noreply.github.com>
2023-04-04 23:07:36 +0300

    Parse Deno response and get error in case of failure

--- src/devneya/exec.clj
+++ src/devneya/exec.clj
@@ -1,14 +1,25 @@
 (ns devneya.exec
   (:require [devneya.utils :as utils]
-            [babashka.process :as bp]))
+            [babashka.process :as bp]
+            [devneya.err :as err]))
 
 (defn exec-code
   "Execute file in Deno"
-  ([deno-token deno-project filename] 
-   (println (bp/shell (str "deployctl deploy --token=" deno-token " --project=" deno-project " " filename)))
-   )
-  
-  ([filename] (let [config (utils/load-config "config.yml")] 
-                (println (bp/shell (str "deployctl deploy --token=" (:deno-token config) " --project=" (:deno-project config) " " filename))))
-   )
+  ([deno-token deno-project filename]
+    (try
+      (let [response (bp/shell {:err "error.txt"} (str "deployctl deploy --token=" deno-token " --project=" deno-project " " filename))]
+        (println "Success")
+      )
+    (catch Throwable e (println "Failed to execute"))
+    ) 
   )
+  
+  ([filename] (let [config (utils/load-config "config.yml")]
+    (try
+      (let [response (bp/shell {:err "error.txt"} (str "deployctl deploy --token=" (:deno-token config) " --project=" (:deno-project config) " " filename))]
+        (println "Success")
+      )
+      (catch Throwable e (println "Failed to execute"))
+    )
+  ))
+)
