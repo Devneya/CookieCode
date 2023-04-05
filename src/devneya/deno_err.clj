1fc273b0b619768ec7ae47c2c411ac36274c546d
Артём <artemyipeshkov@gmail.com>
2023-04-05 17:15:32 +0300

    deno error handler version 1.02

--- src/devneya/deno_err.clj
+++ src/devneya/deno_err.clj
@@ -4,9 +4,9 @@
             )
 
 
-(defn remove-spaces
-  [stri]
-  (clstr/replace (clstr/trim stri) #"([\s]+)" " "))
+;; (defn remove-spaces
+;;   [stri]
+;;   (clstr/replace (clstr/trim stri) #"([\s]+)" " "))
 
 (defn remove-colors
   [stri]
@@ -19,13 +19,16 @@
   [stri output-path]
   (let [output-with-underline (clstr/replace output-path #"." #(str "[_" (if (or (= %1 "\\") (= %1 "/")) "\\/" %1) "]")),
         ;;regexp parsing: "at 'any part of path before output-path entrance' output-path :num1:num2"" num1 and num2 are string and character
-        re-bad-string (re-pattern (str "([\\s^]*)at ([_\\S]*)" output-with-underline "[_:]([_\\d]+)[_:]([_\\d]+)")),
-        data-to-update (re-find re-bad-string stri)]
-    ;; TODO: add correct '\r\n' or '\n' according to the platform 
-    (clstr/replace stri re-bad-string  (str " Error starts at string " (get data-to-update 3) " char " (get data-to-update 4)))))
+        re-bad-string (re-pattern (str "([\\s^]*)at ([_\\S]*)" output-with-underline "[_:]([_\\d]+)[_:]([_\\d]+)"))
+        ;;number of group in regexp that matches number of string where error starts
+        index-of-string-number 3,
+        ;;number of group in regexp that matches position in string where error starts
+        index-of-char-number 4]
+    ;; TODO: add correct '\r\n' or '\n' according to the platform  
+    (clstr/replace stri re-bad-string #(str " Error starts at string " (get %1 index-of-string-number) " char " (get %1 index-of-char-number)))))
 
 (defn deno-error-formatter
   [file-path]
-  (spit utils/current-deno-error-path (remove-user-path (remove-colors (remove-spaces (slurp utils/current-deno-error-path))) file-path))
+  (spit utils/current-deno-error-path (remove-user-path (remove-colors (slurp utils/current-deno-error-path)) file-path))
   )
 
