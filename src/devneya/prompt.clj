c0412b970b0aef2b0a495f3b8e54d9f5b0f19d76
Mell0r <malovtarasov@gmail.com>
2023-04-05 17:14:34 +0300

    hotfix: turns out, that i don't know how to write if body in clojure

--- src/devneya/prompt.clj
+++ src/devneya/prompt.clj
@@ -3,5 +3,6 @@
 
 (defn make-prompt
   "Send prompt to write a code to AI and saves result in *output-filename* file"
-  [config prompt output-filename]
-  (spit output-filename (gpt/get-chatgpt-api-response prompt (:api-key config))))
+  [api-key prompt output-filename]
+  (let [response (gpt/get-chatgpt-api-response prompt api-key)]
+    (spit output-filename response)))
