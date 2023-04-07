1d2f90622dd22722623bda270fe28c1b9f81d6df
Igor Kiselev <kigorleo@gmail.com>
2023-04-07 22:53:10 +0300

    feat: modify request to chatGPT

--- src/devneya/prompt.clj
+++ src/devneya/prompt.clj
@@ -5,7 +5,7 @@
 (defn make-prompt
   "Send prompt to write a code to AI and saves result in *output-filename* file"
   [api-key prompt output-filename]
-  (let [response (gpt/get-chatgpt-api-response prompt api-key)]
+  (let [response (gpt/get-chatgpt-api-response (str prompt " Write only code. Do not use ```.") api-key)]
     (spit output-filename response)))
 
 (defn make-gpt-fix-request 
