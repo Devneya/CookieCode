4fce55abb81f2323978b94e7f3e62dd964d8865d
Igor Kiselev <kigorleo@gmail.com>
2023-04-07 15:50:57 +0300

    feat: in case of deployctl failure automaticaly starts auto-fix

--- src/devneya/exec.clj
+++ src/devneya/exec.clj
@@ -2,21 +2,31 @@
   (:require [devneya.utils :as utils]
             [devneya.deno_err :as dener]
             [babashka.process :as bp]
-            [devneya.err :as err]))
+            [devneya.err :as err]
+            [devneya.prompt :as prompt]))
+
+(defn handle-error 
+  "Generates gpt fix request"
+  [filename e]
+  (err/show-error e)
+  (dener/deno-error-formatter filename)
+  (println "Retrying...")
+  (prompt/make-gpt-fix-request filename)
+  )
 
 (defn exec-code
   "Execute file in Deno"
   ([deno-token deno-project filename]
     (try 
-      (bp/shell {:err utils/current-deno-error-path} (str "deployctl deploy --token=" deno-token " --project=" deno-project " " filename)) 
-      (catch Throwable e (err/show-error e)) 
-      (finally (dener/deno-error-formatter filename))
+      (bp/shell {:err utils/current-deno-error-path} (str "/Users/igorkiselev/.deno/bin/deployctl deploy --token=" deno-token " --project=" deno-project " " filename)) 
+      (catch Throwable e (handle-error filename e)) 
+    ;;  (finally (make-gpt-fix-request filename))
     ) 
   )
   
   ([filename] (let [config (utils/load-config "config.yml")]
     (try 
-      (bp/shell {:err utils/current-deno-error-path} (str "deployctl deploy --token=" (:deno-token config) " --project=" (:deno-project config) " " filename))
+      (bp/shell {:err utils/current-deno-error-path} (str "/Users/igorkiselev/.deno/bin/deployctl deploy --token=" (:deno-token config) " --project=" (:deno-project config) " " filename))
       (catch Throwable e (err/show-error e))
       (finally (dener/deno-error-formatter filename))
     )
--- src/devneya/prompt.clj
+++ src/devneya/prompt.clj
@@ -1,8 +1,19 @@
 (ns devneya.prompt
-  (:require [devneya.gpt :as gpt]))
+  (:require [devneya.gpt :as gpt]
+            [devneya.utils :as utils]))
 
 (defn make-prompt
   "Send prompt to write a code to AI and saves result in *output-filename* file"
   [api-key prompt output-filename]
   (let [response (gpt/get-chatgpt-api-response prompt api-key)]
     (spit output-filename response)))
+
+(defn make-gpt-fix-request 
+  "Generates gpt fix request"
+  [filename]
+  (def prompt (str "Here is a code:\n" (slurp filename) "\nAn error occurred while executing this code:\n" (slurp "deno_error.txt") 
+  "Rewrite code to fix it. Write only code. Do not use ```."))
+  (def config (utils/load-config "config.yml"))
+  (let [response (gpt/get-chatgpt-api-response prompt (:api-key config))]
+       (spit filename response))
+  )
