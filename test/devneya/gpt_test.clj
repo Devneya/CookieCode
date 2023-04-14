8f4ad861ee4722c28e14a7d47b7d8f4fc3ec4ed0
Mell0r <malovtarasov@gmail.com>
2023-04-14 16:41:52 +0300

    test: correct querry to gpt

--- test/devneya/gpt_test.clj
+++ test/devneya/gpt_test.clj
@@ -1,7 +1,11 @@
 (ns devneya.gpt-test
   (:require [clojure.test :refer :all]
-            [devneya.gpt :refer :all :as gpt]))
+            [devneya.gpt :as gpt]
+            [devneya.core :as core]))
 
 (deftest get-chatgpt-api-response-test
   (testing "Exception thrown when openai api key is wrong."
-    (is (thrown? Exception (gpt/get-chatgpt-api-response "hello" "wrong key!")))))
+    (is (thrown? Exception (gpt/get-chatgpt-api-response "hello" "wrong key!"))))
+  (testing "Correct querry to openai api shouldn't lead to exception."
+    (gpt/get-chatgpt-api-response "write hello world" (:OPENAI_KEY core/config)))
+  )
