fb61098d300dc6ab12602fb997942237c1dca497
Setom29 <sergeymtolstov35@gmail.com>
2023-04-21 21:16:17 +0300

    refactor: exec

--- src/devneya/exec.clj
+++ src/devneya/exec.clj
@@ -15,8 +15,6 @@
   ([config]
    
    (when (.exists (io/file (:DENO_ERROR_FILENAME config))) (io/delete-file (:DENO_ERROR_FILENAME config)))
-   (println (str (get-deployctl-command) " deploy --token=" (:DENO_DEPLOY_TOKEN config) " --project=" (:DENO_PROJECT config) " " (:CODE_FILENAME config)))
    (try
-     
-     (bp/shell {:DENO_ERROR_FILENAME config} (str (get-deployctl-command) " deploy --token=" (:DENO_DEPLOY_TOKEN config) " --project=" (:DENO_PROJECT config) " " (:CODE_FILENAME config)))
+     (bp/shell {:err (:DENO_ERROR_FILENAME config)} (str (get-deployctl-command) " deploy --token=" (:DENO_DEPLOY_TOKEN config) " --project=" (:DENO_PROJECT config) " " (:CODE_FILENAME config)))
      (catch Throwable e (spit (:DENO_ERROR_FILENAME config) e)))))
