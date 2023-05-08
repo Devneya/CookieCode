cb917a000f09ecbdd537620617e47e1b4c5135ab
Mell0r <malovtarasov@gmail.com>
2023-05-08 15:29:58 +0400

    fix: submit button is working and copying prompt into response now

--- src/main/devneya/react.cljs
+++ src/main/devneya/react.cljs
@@ -1,18 +1,32 @@
 (ns devneya.react
-  (:require [reagent.dom :as rdom]))
+  (:require [reagent.dom :as rdom]
+            [reagent.core :as r]))
 
 (defn- request-page []
-  [:div
-   [:header
-    [:h1 "Devneya"]]
-   [:main
-    [:section
-     [:h2 "Enter prompt"]
-     [:form {:action "" :method "post"}
-      [:label {:for "prompt"} "Prompt:"]
-      [:input {:type "text" :id "prompt" :name "prompt" :required true}]
-      [:br]
-      [:input {:type "submit" :value "Submit"}]]]]])
+  (let [prompt (r/atom "")
+        response (r/atom "")]
+    (fn []
+      [:div
+       [:header
+        [:h1 "Devneya"]]
+       [:main
+        [:section
+         [:h2 "Enter prompt"]
+         [:label {:for "prompt"} "Prompt:"]
+         [:input {:type "text"
+                  :id "prompt"
+                  :name "prompt"
+                  :value @prompt
+                  :on-change #(reset! prompt (-> % .-target .-value))}]
+         [:br]
+         [:button
+          {:type "submit"
+           :on-click #(reset! response @prompt)}
+          "Submit"]]
+        [:input {:type "text"
+                 :id "response"
+                 :name "response"
+                 :value @response}]]])))
 
 (defn run-react []
   (rdom/render
