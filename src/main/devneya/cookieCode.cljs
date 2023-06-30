e281859d29bca21a7c684516a8ec9ba875bd8d65
Setom29 <sergeymtolstov35@gmail.com>
2023-06-30 11:32:51 +0300

    fix: copy button

--- src/main/devneya/cookieCode.cljs
+++ src/main/devneya/cookieCode.cljs
@@ -37,13 +37,13 @@
 
 (defn add-popup-toggle-class-listener []
   (let [cookie-code-form (get-element-by-class-name "cookie-code-form")
-        cookie-code-form__close-button (get-element-by-class-name "cookie-code-form__close-button")]
+        cookie-code-form__close-button (get-element-by-class-name "cookie-code-form__close-button")
+        cookie-code-form__copy-button (get-element-by-class-name "cookie-code-form__copy-button")]
     (.addEventListener cookie-code-popup-button "click" #(toggle-class cookie-code-form "active"))
-    (.addEventListener cookie-code-form__close-button "click" #(toggle-class cookie-code-form "active"))))
+    (.addEventListener cookie-code-form__close-button "click" #(toggle-class cookie-code-form "active"))
+    (.addEventListener cookie-code-form__copy-button "click" #((. (. js/navigator -clipboard) writeText (.-value (get-element-by-class-name "cookie-code-form__response")))
+                                                               (toggle-class cookie-code-form "active")))))
 
 (defn render-cookie-code []
   (rdom/render [cookie-code-form-template] cookie-code-container)
   (add-popup-toggle-class-listener))
-
-
-
