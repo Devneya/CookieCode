a428ea2beba0595022d6085b4c3c082ed3827df0
Mell0r <malovtarasov@gmail.com>
2023-07-25 14:04:56 +0400

    fix: test back to normal, added TODO to fix Github Action

--- src/test/devneya/promptTest.cljs
+++ src/test/devneya/promptTest.cljs
@@ -38,7 +38,7 @@
                     "")]
       (async done
              (go
-               (is (= (<! res-chan) "неверно"))
+               (is (= (<! res-chan) "console.log(\"Hello, world!\");"))
                (is (= ((::call-count (meta hello-world-returner))) 1))
                (done))))))
   
